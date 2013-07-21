7e506527135ea5a6effe0b57a0406305a134f4d1
agonia <agonia.suh@gmail.com>
2013-07-21 23:54:41 +0900

    Initial Commit

---
+++ memorygame/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  memorygameAppController.h
+//  memorygame
+//
+//  Created by hwani5 on 13. 7. 21..
+//  Copyright __MyCompanyName__ 2013년. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ memorygame/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  memorygameAppController.h
+//  memorygame
+//
+//  Created by hwani5 on 13. 7. 21..
+//  Copyright __MyCompanyName__ 2013년. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
